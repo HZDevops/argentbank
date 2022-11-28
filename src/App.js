c15652afb3ae3c1c68cd2dfa20dcbe755150ca5e
Zoubeiri <housna.zoubeiri@gmail.com>
2022-11-28 10:50:55 +0100

    Create SignIn component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import "./components/FeatureItem/FeatureItem";
-import image from "./assets/icon-chat.png";
-import FeatureItem from "./components/FeatureItem/FeatureItem";
+
 function App() {
-  return (
-    <div className="App">
-      <FeatureItem image={image} title="Hello" text="text" />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./SignIn.css";
+
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="sign-in-content">
+      <i className="fa fa-user-circle sign-in-icon"></i>
+      <h1>Sign In</h1>
+      <form onSubmit={submitHandler}>
+        <div className="input-wrapper">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-remember">
+          <input type="checkbox" id="remember-me" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <button className="sign-in-button" type="submit" name="Login">
+          Sign In
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default SignIn;
